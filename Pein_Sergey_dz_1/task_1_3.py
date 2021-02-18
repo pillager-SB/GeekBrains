49d567162f97ba8f7e3c6d24a9bc332c7da22378
Sergey Petin <pillager@bk.ru>
2021-02-18 09:45:58 +0300

    Домашние задания к лекции 1

--- Pein_Sergey_dz_1/task_1_3.py
+++ Pein_Sergey_dz_1/task_1_3.py
@@ -1,11 +1,13 @@
 # 1-процент, 2 - 4 - процента, 0,5 - 20 - процентов
+
 name = ''
+
 for num in range(0, 21):
 
-    if num == 1:
-        name = 'процент'
-    elif 2 <= num <= 4:
+    if 2 <= num <= 4:
         name = 'процента'
+    elif num == 1:
+        name = 'процент'
     else:
         name = 'процентов'
 
